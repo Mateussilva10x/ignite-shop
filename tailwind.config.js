79e297a451bf866e5bdfa45ed48feba774eff9a7
Mateus Silva <mateussilva651@gmail.com>
2023-05-25 10:16:42 -0300

    adjusted the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,23 +16,24 @@ module.exports = {
       green200: "#1ea483",
       black600: "rgba(0,0,0,0.6)",
     },
-    maxWidth: {
-      shopScreen: "calc(100vw - ((100vw - 1180px) / 2 ))",
+    extend: {
+      fontSize: {
+        md: "1.125rem",
+        lg: "1.25rem",
+        xl: "1.5rem",
+        "2xl": "2rem",
+      },
+      maxWidth: {
+        shopScreen: "calc(100vw - ((100vw - 1180px) / 2 ))",
+      },
+      minHeight: {
+        shopScreen: 656,
+      },
+      translate: {
+        outWindow: "110%",
+        inWindow: "0%",
+      },
     },
-    minHeight: {
-      shopScreen: 656,
-    },
-    fontSize: {
-      md: "1.125rem",
-      lg: "1.25rem",
-      xl: "1.5rem",
-      "2xl": "2rem",
-    },
-    translate: {
-      outWindow: "110%",
-      inWindow: "0%",
-    },
-    extend: {},
   },
   plugins: [],
 };
